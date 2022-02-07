c306383c48c9ed8725b2b1ea49b620fe1f18b317
mahendran29 <mithu.dar@gmail.com>
2022-02-07 17:27:49 +0530

    Made the snake move through the keys

--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -3,6 +3,10 @@ from turtle import Turtle
 
 starting_positions = [(0,0),(-20,0),(-40,0)]
 move_forward = 20
+up = 90
+down = 270
+left = 180
+right = 0
 
 class Snake:
     segments_list = []
@@ -10,6 +14,7 @@ class Snake:
     def __init__(self):
         self.segments_list= []
         self.create_snake()
+        self.head = self.segments_list[0]
     
     def create_snake(self):
         for position in starting_positions:
@@ -24,5 +29,20 @@ class Snake:
              new_x = self.segments_list[seg_num-1].xcor()
              new_y = self.segments_list[seg_num-1].ycor()
              self.segments_list[seg_num].goto(new_x,new_y)
-         self.segments_list[0].forward(20)
-         self.segments_list[0].left(90)
+         self.head.forward(20)
+    
+    def up(self):
+        if self.head.heading() != down:
+            self.head.setheading(up)
+
+    def down(self):
+        if self.head.heading() != up:
+            self.head.setheading(down)
+        
+    def left(self):
+        if self.head.heading() != right:
+            self.head.setheading(left)
+
+    def right(self):
+        if self.head.heading() != left:
+            self.head.setheading(right)
